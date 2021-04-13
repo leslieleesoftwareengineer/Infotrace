d4b70dc74af1e8cab9187f7b18e23a9c296479c9
leslieleesoftwareengineer <80076295+leslieleesoftwareengineer@users.noreply.github.com>
2021-04-13 11:13:45 +1000

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,3 +1,5 @@
+dotnet dev-certs https -ep %USERPROFILE%\.aspnet\https\aspnetapp.pfx -p 123456
+dotnet dev-certs https --trust
 cd ./InfoTrace.Back-End
 docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d --build
 cd ../InfoTrace.Front-End
